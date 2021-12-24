b4eb513bfd214873aae9345acddc216d68c0ea92
HectorMG02 <hectormatgon@gmail.com>
2021-12-24 13:07:04 +0100

    eliminar clientes y borrarlos de la cache

--- components/Cliente.jsx
+++ components/Cliente.jsx
@@ -1,10 +1,75 @@
-import React from "react";
+import Swal from "sweetalert2";
+import { gql, useMutation } from "@apollo/client";
+
+const GET_CLIENTES_USUARIO = gql`
+  query obtenerClientesVendedor {
+    obtenerClientesVendedor {
+      id
+      nombre
+      apellido
+      email
+      empresa
+      telefono
+    }
+  }
+`;
+
+const ELIMINAR_CLIENTE_SUBMIT = gql`
+  mutation eliminarCliente($id: ID!) {
+    eliminarCliente(id: $id)
+  }
+`;
 
 const Cliente = ({ cliente }) => {
+  const [eliminarClienteSubmit] = useMutation(ELIMINAR_CLIENTE_SUBMIT, {
+    update(cache) {
+      const { obtenerClientesVendedor } = cache.readQuery({
+        query: GET_CLIENTES_USUARIO,
+      });
+
+      cache.writeQuery({
+        query: GET_CLIENTES_USUARIO,
+        data: {
+          obtenerClientesVendedor: obtenerClientesVendedor.filter(
+            (clienteActual) => clienteActual.id !== cliente.id
+          ),
+        },
+      });
+    },
+  });
   const { nombre, apellido, email, empresa, id } = cliente;
 
   const eliminarCliente = (id) => {
-    console.log({ id });
+    Swal.fire({
+      title: `¿Estas seguro de que quieres eliminar el cliente?\n\n <u>&bull;${nombre} ${apellido}</u>`,
+      text: "Esta acción no se puede deshacer",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      console.log({ id });
+      if (result.value) {
+        try {
+          const { data } = await eliminarClienteSubmit({
+            variables: {
+              id,
+            },
+          });
+
+          Swal.fire(
+            "Eliminado!",
+            `El cliente ${nombre} ${apellido} de la empresa ${empresa} ha sido eliminado con éxito.`,
+            "success"
+          );
+        } catch (error) {
+          console.log(error);
+          Swal.fire("Error", "No se pudo eliminar el cliente", "error");
+        }
+      }
+    });
   };
 
   return (
@@ -20,16 +85,16 @@ const Cliente = ({ cliente }) => {
         >
           Eliminar
           <svg
-            class="w-6 h-6"
+            className="w-6 h-6"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
             ></path>
           </svg>
